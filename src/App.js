83fa66495a8d16746ec56d52458c6ef785b8a9ee
ariabd-ac <ariabd.com@gmail.com>
2020-07-18 18:53:21 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import iconurl from './icon/爱心.png'
 import Main from './Main'
@@ -13,20 +15,35 @@ class App extends Component {
 
     const main = () => {
       if (this.state.show)
-        return <Main />
+        return <Main / >
     }
-    return (
-      <div>
-        <div className={"envelope animated " + this.state.class}>
-          <div className="triangle-down"></div>
-          <img className="heart" src={iconurl} onClick={() => {
-            this.setState({ class: 'bounceOutRight', show: true })
-          }} />
-          <div className="triangle-up"></div>
-          <div className="text">你收到一封信，点击查收！</div>
-        </div>
-        {main()}
-      </div>
+    return ( <
+      div >
+      <
+      div className = {
+        "envelope animated " + this.state.class
+      } >
+      <
+      div className = "triangle-down" > < /div> <
+      img className = "heart"
+      src = {
+        iconurl
+      }
+      onClick = {
+        () => {
+          this.setState({
+            class: 'bounceOutRight',
+            show: true
+          })
+        }
+      }
+      /> <
+      div className = "triangle-up" > < /div> <
+      div className = "text" > Hei sayang, buka ya. < /div> <
+      /div> {
+        main()
+      } <
+      /div>
     );
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 /**
  * Created by Rychou on 2018/4/19.
  */
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import $ from 'jquery'
-import url from './audio/gbqq.mp3' // 引入背景音乐文件
-
+// import url from './audio/gbqq.mp3' // 引入背景音乐文件
+import url from './audio/eds.mp3'
 
 class Main extends Component {
     state = {
@@ -13,9 +15,8 @@ class Main extends Component {
     componentDidMount() {
         this.print();
         setInterval(() => {
-            this.time(2016, 4, 23) // 你们的纪念日
-        }, 1000
-        )
+            this.time(2014, 7, 14) // 你们的纪念日
+        }, 1000)
         var audio = document.getElementById("audio");
         setTimeout(() => audio.play(), 8500) // 背景音乐在页面加载后，延迟播放的时长，单位：毫秒。
     }
@@ -33,8 +34,7 @@ class Main extends Component {
                 // html标签完整输出,如：<p>
                 if (current == '<') {
                     index = str.indexOf('>', index) + 1;
-                }
-                else {
+                } else {
                     index++;
                 }
                 //位运算符: 根据setInterval运行奇偶次来判断是否加入下划线字符“_”，使输入效果更逼真
@@ -59,48 +59,73 @@ class Main extends Component {
         var hour = parseInt(((dateNow - dateJNR) / (3600 * 1000)) % 24);
         var minute = parseInt((dateNow - dateJNR) / (1000 * 60) % 60);
         var second = parseInt((dateNow - dateJNR) / 1000 % 60);
-        this.setState({ date: { d: d, hour: hour, minute: minute, second: second } });
+        this.setState({
+            date: {
+                d: d,
+                hour: hour,
+                minute: minute,
+                second: second
+            }
+        });
     };
     render() {
         const date = () => {
             if (this.state.date.d !== undefined) {
-                const { d, hour, minute, second } = this.state.date
-                return (<p>我们已经一起走过了: <span className="date-text">{d}</span> 天 <span className="date-text">{hour}</span> 小时 <span className="date-text">{minute}</span> 分 <span className="date-text">{second}</span> 秒 </p>
-                )
+                const {
+                    d,
+                    hour,
+                    minute,
+                    second
+                } = this.state.date
+                return ( < p > Kita telah bersama selama: < span className = "date-text" > {
+                    d
+                } < /span> hari <span className="date-text">{hour}</span > jam < span className = "date-text" > {
+                    minute
+                } < /span> menit <span className="date-text">{second}</span > detik < /p>)
             }
         }
-        return (
-            <div className="App animated bounceInLeft">
-                <div className="date">{date()}</div>
-                <div id="autotype">
-                    <h1 style={{ fontWeight: 900 }}>哈喽！wuli趴布猪！</h1>
-                    <p >在煽情开始之前，先放首歌当背景音乐吧！Music!</p>
-                    <p>今天是我们两周年的纪念日，从2016年4月23日到现在，我们一起经历了许许多多的事情，
-                    有欢笑也有争吵，也曾因为一些事情闹过分手，但是我们都走过来了。</p>
-                    <p>高三那会，每天都期待着见到你，上课的时候视线会不自觉的到你身上；吃早餐帮你打好早餐，帮你洗早餐盒；
-                        下课的时候假装凑到你身边看看你在干啥，有没有不会的题目，然后就可以教你做题，表面上是想展现
-                        自己的学霸风采。实际上啊，是因为喜欢和你呆在一起。喜欢看你的甜美的笑容，喜欢轻轻地闻你的发香，
-                        慢慢地开始喜欢你身上的一切。当从别人口中得知你喜欢我这句话时候，我那一整天都笑的合不拢嘴，晚上也睡不着，脑海里一直重复着那句话。
-                </p>
-                    <p>在4月23日那天晚上，在书店，我还记得清清楚楚，你穿着T恤和短裤，头发还散发着清香，我的心好像有什么感应一样，一靠近你就开始加速跳动。
-                        在回去的路上，我正要说出“我喜欢你”这四个字的时候，你突然打断我说：“我也是！”。那一刻感觉时间都静止了，
-                        整个人都处于一种飘飘然的状态。再到后来高考后的那个暑假，也是我一生当中最难忘的暑假，初吻、约会、每天都歪腻在一起，真心的感谢你陪我度过的那些时光。
-                </p>
-                    <p>然后我们就开始了漫长的异地恋，还记得我们异地后的第一次见面嘛，分别那天傍晚，一个人开着电动车哭到哽咽，口头上说着没事，但没想到分别竟然是如此刺痛，那时候我就决定，
-                        “我魏锐，非汪雅秋不娶”。在异地的期间里，我们一起去过很多地方，留下过很多难忘的回忆。有人说：“异地恋其实并不难坚持，因为它真实地考验了
-                        两个人的心”。是啊！我们异地经历的那些事，让我们能够看清对方的真心，也让我们的感情更加深厚。最近很喜欢《谁愿放手》中的一句话，“年年月月逝去越是觉得深爱你”。
-                </p>
-                    <p>我不善于文字表达，写这一个网页和说那么多话也就想表达“我爱你”这简单的三个字而已，说煽情一点就是：“我的心室是单人间，只住得下你一人！”
-                        好期待以后结束异地后的同居生活啊，希望那一天早点到来。
-                </p>
-                    <p>最后祝wuli趴布猪两周年纪念日快乐哦！</p>
-                    <div style={{ textAlign: 'right' }}>
-                        <p>爱你的♥魏锐</p>
-                        <p>2018年4月23日</p>
-                    </div>
-                </div>
-                <audio id="audio" src={url}></audio>
-            </div>
+        return ( <
+            div className = "App animated bounceInLeft" >
+            <
+            div className = "date" > {
+                date()
+            } < /div> <
+            div id = "autotype" >
+            <
+            h1 style = {
+                {
+                    fontWeight: 900
+                }
+            } > Hi Bby < /h1> <
+            p > Sebelum dimulai, ayo puter lagunya dulu wkwk < /p> <
+            p > Selasa, 14 Juni 2020 adalah hari peringatan jadian kita yang ke 6 Tahun.Kita sudah cukup lama ya menghadapi segala sesuatu bersama bb hehe <
+            /p> <
+            p > Sebelumnya, aku ingin minta maaf ya buat yang kesalahan aku kemarin, maafin yaaa maafin dong kamu kan baik ^ ^
+            <
+            /p> <
+            p > Hey sayang, 6 tahun bukan waktu yang sebentar untuk bersama - sama.Dari kita masih mungil kecil sekarang jadi rada buncit, rada loh ya: v aku gatau mau nulis apa, dan gimana, aku ga pandai merangkai kata seperti kamu yang pintar membuat dan merangkai kata yang bisa uwu. <
+            /p> <
+            p > Yaudah, intinya Selamat hari jadian kita yang ke - 6 tahun ya, Wish nya yang deket dulu deh untuk kita punya pekerjaan yang layak dan halal,
+            terus doakan aku supaya cepat lulus dan menghasilkan uang untuk bisa cepet - cepet lebih ke jenjang yang serius. <
+            /p> <
+            p > Izinkan aku buat jagain hubungan ini lebih lama lagi, dan semoga rasa kamu masih sama dengan aku seterusnya sampe aku ga buncit lagi donggg wkwkwk <
+            /p> <
+            p > Sekali lagi, selamat hari jadian yang ke - 6 tahun ya！ Love ya sayang < /p> <
+            div style = {
+                {
+                    textAlign: 'right'
+                }
+            } >
+            <
+            p > Pacarmu yang ganteng < /p> <
+            p > 14 Juni 2020 < /p> <
+            /div> <
+            /div> <
+            audio id = "audio"
+            src = {
+                url
+            } > < /audio> <
+            /div>
 
         )
     }
